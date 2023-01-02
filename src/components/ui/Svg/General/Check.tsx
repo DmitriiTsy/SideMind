b9d03bca86b8996f8291137720a26cc34a9550d1
Darcless1 <dmitry.tsyplakov.works@gmail.com>
2023-01-02 11:50:27 -0800

    Check_icon_import

---
+++ src/components/ui/Svg/General/Check.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react'
+
+import Svg from 'react-native-svg'
+
+import { SvgStyleProps } from 'components/ui/Svg/Svg.types'
+import { createSvg } from 'utils/createSvg'
+
+const Component: FC<SvgStyleProps> = (props) => (
+  <Svg width={20} height={21} fill="none" {...props}>
+    <rect x={0.5} y={1} width={19} height={19} rx={9.5} fill="#559EF8" />
+    <path d="m5 10.5 3 3 7-7" stroke="#1C1C1D" />
+    <rect x={0.5} y={1} width={19} height={19} rx={9.5} stroke="#484849" />
+  </Svg>
+)
+
+export const CheckIcon = createSvg({
+  component: Component,
+  originalHeight: 21,
+  originalWidth: 20
+})
--- src/components/ui/Svg/constants.ts
+++ src/components/ui/Svg/constants.ts
@@ -1,5 +1,8 @@
 import { SvgLogo } from 'components/ui/Svg/General'
 
+import { CheckIcon } from '../Svg/General/Check'
+
 export const SVG_MAP = {
-  Logo: SvgLogo
+  Logo: SvgLogo,
+  Check: CheckIcon
 }
