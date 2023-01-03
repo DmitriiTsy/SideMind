dd5f905f235df3bb6c972b1893d5025b2c8c0210
nikita <nkalmykov937@gmail.com>
2023-01-03 20:49:30 +0600

    review map bots

--- src/components/SelectSideMinds/constants.ts
+++ src/components/SelectSideMinds/constants.ts
@@ -1,126 +1,148 @@
-import { EBotsCategory, IBotsMap } from 'components/SelectSideMinds/types'
+import { IBot } from 'components/SelectSideMinds/types'
 
-export const BOTS_MAP: IBotsMap = {
-  [EBotsCategory.None]: [
+export const BOTS_MAP: IBot[][] = [
+  [
     {
       name: 'SideMind Sam',
       tagLine: 'Welcome! Let me show you around.',
-      prompt: ''
+      prompt: '',
+      category: ''
     },
     {
       name: 'Feedback Fiona',
       tagLine: 'Wanna report a bug or have a feature request?',
-      prompt: ''
+      prompt: '',
+      category: ''
     }
   ],
-  [EBotsCategory.SelfImprovement]: [
+  [
     {
       name: 'Roxy the Relaxer',
       tagLine: 'Welcome! Let me show you around.',
-      prompt: ''
+      prompt: '',
+      category: ''
     },
     {
       name: 'Journaling Jamie',
       tagLine: 'Wanna report a bug or have a feature request?',
-      prompt: ''
+      prompt: '',
+      category: ''
     },
     {
       name: 'Self-Help Sally',
       tagLine: 'Wanna report a bug or have a feature request?',
-      prompt: ''
+      prompt: '',
+      category: ''
     }
   ],
-  [EBotsCategory.Productivity]: [
+  [
     {
       name: 'CV Claudia',
       tagLine: 'Cover letter success, guaranteed! ',
-      prompt: ''
+      prompt: '',
+      category: ''
     },
     {
       name: 'Essay Elaine',
       tagLine: 'Let me craft the perfect essay for you.',
-      prompt: ''
+      prompt: '',
+      category: ''
     },
     {
       name: 'Encyclopedia Emily',
       tagLine: 'From A to Z, I got you covered.',
-      prompt: ''
+      prompt: '',
+      category: ''
     },
     {
       name: 'Influencer Isla',
       tagLine: 'Let me help you become an Instagram sensation! ',
-      prompt: ''
+      prompt: '',
+      category: ''
     }
   ],
-  [EBotsCategory.Hobbies]: [
+  [
     {
       name: 'Creative Charlie',
       tagLine: 'What are you working on?',
-      prompt: ''
+      prompt: '',
+      category: ''
     },
     {
       name: 'Chef Chrissy',
       tagLine: 'Your taste buds will thank you',
-      prompt: ''
+      prompt: '',
+      category: ''
     },
     {
       name: 'Adventure Alex',
       tagLine: 'Let me help you plan the adventure of a lifetime!',
-      prompt: ''
+      prompt: '',
+      category: ''
     }
   ],
-  [EBotsCategory.Relationships]: [
+  [
     {
       name: 'Social Sawyer',
       tagLine: 'Become a pro at communication with our guidance.',
-      prompt: ''
+      prompt: '',
+      category: ''
     },
     {
       name: 'Loving Lee',
       tagLine: 'Embrace the power of being loved',
-      prompt: ''
+      prompt: '',
+      category: ''
     },
     {
       name: 'Life Coach Larry',
       tagLine: "Feeling stuck? Let's get back on track!",
-      prompt: ''
+      prompt: '',
+      category: ''
     },
     {
       name: 'Tommy Tinder',
       tagLine: "Let's improve your Tinder game.",
-      prompt: ''
+      prompt: '',
+      category: ''
     }
   ],
-  [EBotsCategory.Entertainment]: [
+  [
     {
       name: 'Hungry Henry',
       tagLine: 'Save me from starvation with your help!',
-      prompt: ''
+      prompt: '',
+      category: ''
     },
     {
       name: 'Nervous Nelly',
       tagLine: 'Together, we can conquer anxiety, one deep breath at a time. ',
-      prompt: ''
+      prompt: '',
+      category: ''
     },
     {
       name: 'Miami Greg',
       tagLine: "Hold on.. I'm at the club.",
-      prompt: ''
+      prompt: '',
+      category: ''
     },
     {
       name: 'Cory the Bad Comedian',
       tagLine: 'Why did the chicken.. oh silence.',
-      prompt: ''
+      prompt: '',
+      category: ''
     },
     {
       name: 'Negative Nancy',
       tagLine: 'Stay away.',
-      prompt: ''
+      prompt: '',
+      category: ''
     },
     {
       name: 'Ball Buster Bob',
       tagLine: "I'm here to make you laugh - even if it's at your own expense!",
-      prompt: ''
+      prompt: '',
+      category: ''
     }
   ]
-}
+]
--- src/components/SelectSideMinds/types.ts
+++ src/components/SelectSideMinds/types.ts
@@ -1,23 +1,6 @@
-export enum EBotsCategory {
-  None = 'None',
-  SelfImprovement = 'Self-Improvement',
-  Productivity = 'Productivity',
-  Hobbies = 'Hobbies',
-  Relationships = 'Relationships',
-  Entertainment = 'Entertainment'
-}
-
-interface IBot {
+export interface IBot {
   name: string
   tagLine: string
   prompt: string
-}
-
-export interface IBotsMap {
-  [EBotsCategory.None]: IBot[]
-  [EBotsCategory.SelfImprovement]: IBot[]
-  [EBotsCategory.Productivity]: IBot[]
-  [EBotsCategory.Hobbies]: IBot[]
-  [EBotsCategory.Relationships]: IBot[]
-  [EBotsCategory.Entertainment]: IBot[]
+  category: string
 }
