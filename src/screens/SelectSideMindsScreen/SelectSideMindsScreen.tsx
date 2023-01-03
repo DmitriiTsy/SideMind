f4dcb9ffcd41bc75f4aa1607e61faf03131fde88
nikita <nkalmykov937@gmail.com>
2023-01-03 19:43:02 +0600

    added SelectSideMinds screen

---
+++ src/screens/SelectSideMindsScreen/SelectSideMindsScreen.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import { SelectSideMinds } from 'components/SelectSideMinds'
+
+export const SelectSideMindsScreen = () => {
+  return <SelectSideMinds />
+}
--- src/screens/SelectSideMindsScreen/index.ts
+++ src/screens/SelectSideMindsScreen/index.ts
@@ -1,2 +1,1 @@
 export * from './SelectSideMindsScreen'
-export * from './BootScreen'
