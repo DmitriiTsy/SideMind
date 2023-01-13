85b50d05a042e30be8553a36853c5ce76e6508b5
Darcless1 <dmitry.tsyplakov.works@gmail.com>
2023-01-12 18:04:47 -0800

    params_func

--- src/services/OpenAIService/OpenAIService.ts
+++ src/services/OpenAIService/OpenAIService.ts
@@ -1,24 +1,15 @@
 import { Configuration, OpenAIApi } from 'openai'
 
-import { Injectable, useInject } from 'IoC'
+import { Injectable } from 'IoC'
 import { BotModel } from 'services/FirebaseService/types'
-import { IAppStore, IAppStoreTid } from 'store/AppStore'
 export const IOpenAIServiceTid = Symbol.for('IOpenAIServiceTid')
 
 export interface IOpenAIService {
   init(): void
 
-  createCompletion(prompt: string): Promise<string>
+  createCompletion(prompt: string, bot: BotModel): Promise<string>
 
   clearHistory(): void
-}
-
-export const AppStoreValue = () => {
-  const appStore = useInject<IAppStore>(IAppStoreTid)
-  return appStore
-}
-interface IBotProps {
-  bot: BotModel
 }
 
 @Injectable()
@@ -26,8 +17,7 @@ export class OpenAIService implements IOpenAIService {
   private _config: Configuration
   private _openAIApi: OpenAIApi
   private _history: string
-  appStore = AppStoreValue()
-  
+
   init() {
     this._config = new Configuration({
       apiKey: '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'
@@ -35,16 +25,16 @@ export class OpenAIService implements IOpenAIService {
     this._openAIApi = new OpenAIApi(this._config)
   }
 
-  async createCompletion(prompt: string) {
+  async createCompletion(prompt: string, bot: BotModel) {
     this._history = `${this._history} \n\n###: ${prompt}. \n\n`
     try {
       const res = await this._openAIApi.createCompletion({
         model: 'text-davinci-003',
         prompt: this._history,
-        temperature: 1,
-        max_tokens: 1000,
-        frequency_penalty: 0,
-        presence_penalty: 0.6,
+        temperature: bot.params.temperature,
+        max_tokens: bot.params.max_tokens,
+        frequency_penalty: bot.params.frequency_penalty,
+        presence_penalty: bot.params.presence_penalty,
         stop: ['###']
       })
       this._history = `${this._history} ${res.data.choices[0].text}`
